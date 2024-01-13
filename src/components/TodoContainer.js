3ebf30d01f4cf16870e44bd3cca005b15babe316
abdulrafay-1 <abdulrock413@gmail.com>
2024-01-13 16:45:37 +0500

    some changes

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,58 +1,17 @@
 import TodoItem from './TodoItem'
-import InputForm from './InputForm'
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
 
 
 
 const TodoContainer = () => {
-    useEffect(() => {
-        document.querySelector(".input-form").addEventListener("submit", (e) => e.preventDefault());
-        let todoInput = document.getElementById("todo-input");
-        let submitBtn = document.querySelector(".submit-btn");
-        let checkboxes = document.querySelectorAll(".checkbox");
-        var todoCon = ReactDOM.createRoot(document.getElementById('todo-con'));
-        let todoTitles = document.querySelectorAll(".title");
-       
-        checkboxes.forEach((checkbox, index) => {
-            checkbox.addEventListener("change", () => {
-                if (checkbox.checked) {
-                    todoTitles[index].style.textDecoration = "line-through"
-                } else {
-                    todoTitles[index].style.textDecoration = "none"
-                }
-            })
-        })
-
-
-        function addTodo() {
-            todoCon.render(<TodoItem title={todoInput.value} />);
-        }
-
-        submitBtn.addEventListener("click", addTodo);
-
-        // Clean up event listener on component unmount
-        return () => {
-            submitBtn.removeEventListener("click", addTodo);
-        };
-    }, []);
 
     return (
         <>
             <div className="todo-container">
-                {/* <div>
-                    <button className="add-task">Add Task</button>
-                    <select>
-                        <option>All</option>
-                    </select>
-                </div> */}
-                <InputForm />
                 <div className="todo-list-con" id='todo-con'>
-
-                    <TodoItem title="Create a react Project" />
-                    <TodoItem title="First React Project" />
-                    <TodoItem title="Hello World" />
+                    < TodoItem />
                 </div>
             </div>
         </>
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,19 +1,68 @@
-const TodoItem = (props) => {
+import { useState } from "react";
+
+const TodoItem = () => {
+
+    var [todoItemArr, todoSetter] = useState([])
+    const [inputValue, setInputValue] = useState('')
+    const addTodo = (e) => {
+        e.preventDefault()
+        var todoId = todoItemArr.length ? todoItemArr[todoItemArr.length - 1].todoId + 1 : 51;
+        let createdAt = Date.now()
+        if (!inputValue.trim()) {
+            alert("Please Enter a todo")
+        } else {
+            setInputValue('')
+            const todoItem = {
+                todoId,
+                inputValue,
+                createdAt
+            }
+            todoSetter([...todoItemArr, todoItem])
+        }
+
+    }
+
+    const deleteTodo = (id) => {
+        const updatedTodoItemArr = todoItemArr.filter((todo) => todo.todoId !== id);
+        todoSetter(updatedTodoItemArr);
+    }
+    const updateTodo = (id) => {
+        const findTodo = todoItemArr.find((todo) => todo.todoId === id);
+        const editPrompt = prompt("Edit Todo", findTodo.inputValue)
+        if (!editPrompt || !editPrompt.trim()) {
+            alert("Enter valid todo")
+        } else {
+            findTodo.inputValue = editPrompt
+            todoSetter([...todoItemArr])
+        }
+    }
 
     return (
-        <div className="todo">
-            <div>
-                <input type="checkbox" className="checkbox"></input>
-                <div className="todo-text">
-                    <h4 className="title">{props.title}</h4>
-                    <p>4:20 AM ,01/08/2023</p>
-                </div>
-            </div>
-            <div className='icons-container'>
-                <i class="fa-solid fa-trash"></i>
-                <i class="fa-solid fa-pen"></i>
-            </div>
-        </div>
+        <>
+            <form className='input-form'>
+                <input type='text' id='todo-input' placeholder='Enter text...' value={inputValue} onChange={(e) => setInputValue(e.target.value)}></input>
+                <button className='submit-btn' onClick={addTodo}>Submit</button>
+            </form>
+            {
+                todoItemArr.map((todo) => {
+                    return (
+                        <div className="todo">
+                            <div>
+                                <div className="todo-text">
+                                    <h4 className="title">{todo.inputValue}</h4>
+                                    <p>{new Date(todo.createdAt).toLocaleString()}</p>
+                                </div>
+                            </div>
+                            <div className='icons-container'>
+                                <i className="fa-solid fa-trash" onClick={() => deleteTodo(todo.todoId)}></i>
+                                <i className="fa-solid fa-pen" onClick={() => updateTodo(todo.todoId)}></i>
+                            </div>
+                        </div>
+
+                    )
+                })
+            }
+        </>
     )
 }
 
