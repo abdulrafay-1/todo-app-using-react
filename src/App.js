3ca7fc228efcc2db73dcaf409da8f8bf8e15b846
abdulrafay-1 <abdulrock413@gmail.com>
2024-09-16 01:55:57 +0500

    removed unused code

--- src/App.js
+++ src/App.js
@@ -12,10 +12,6 @@ function App() {
   },[])
 
   useEffect(() => {
-    // const isLocalDark = JSON.parse(localStorage.getItem("isDarkMode"))
-    // if (isLocalDark != null) {
-    //   setDark((isLocalDark))
-    // }
     localStorage.setItem('isDarkMode', dark)
 
   }, [dark])
@@ -33,22 +29,6 @@ function App() {
         <TodoContainer dark={dark} />
       </div>
     </>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
   );
 }
 
